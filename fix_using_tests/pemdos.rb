55d3f898acd8b6853233afb11c51aed14d792989
Saima Rahman <rahman.saima92@gmail.com>
2019-11-15 03:26:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,11 @@
 def snake_it_up(string)
  ten_times = ''
-  if string[0] == "s"
+ result = string.slice(1..-1)
+  10.times{|n|
     ten_times += "s"
+  }
+  if string[0] == "s"
+    ten_times + result
   else
   string
   end
